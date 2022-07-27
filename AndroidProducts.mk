6032d39028487e4e3af75c8349fe98f5ea5adc5c
68orkun <hesaphesapyus@gmail.com>
2022-07-27 14:21:01 +0300

    lava: Adapt for ProjectBlaze

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/aosp_lava.mk
+   $(LOCAL_DIR)/blaze_lava.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lava-user \
-    aosp_lava-userdebug \
-    aosp_lava-eng
+    blaze_lava-user \
+    blaze_lava-userdebug \
+    blaze_lava-eng
