c355794ced2cdabcf4c1d8734d3ec951cab36570
68orkun <hesaphesapyus@gmail.com>
2022-07-27 14:14:38 +0300

    lava: Adapt for Voltage

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/aosp_lava.mk
+   $(LOCAL_DIR)/voltage_lava.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lava-user \
-    aosp_lava-userdebug \
-    aosp_lava-eng
+    voltage_lava-user \
+    voltage_lava-userdebug \
+    voltage_lava-eng
