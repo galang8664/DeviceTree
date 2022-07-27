dba3816689713f2abaf6e98094b9fa3f90193120
68orkun <hesaphesapyus@gmail.com>
2022-07-27 14:09:06 +0300

    lava: Adapt for SparkOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/aosp_lava.mk
+   $(LOCAL_DIR)/spark_lava.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lava-user \
-    aosp_lava-userdebug \
-    aosp_lava-eng
+    spark_lava-user \
+    spark_lava-userdebug \
+    spark_lava-eng
